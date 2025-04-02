848cbca85939fa49319e91aa640c63b73ad3e4b1
bmaupin <bmaupin@users.noreply.github.com>
2025-04-02 13:26:35 -0400

    feat: Start modding victory popup
    For the lua, use the source from textpopup.lua for a simpler base. For the XML we've mostly just cleaned up whitespace, removed the radio buttons, remove the top close button, and replace the bottom accept button with a close button

---
+++ src/Lua/Victories.lua
@@ -0,0 +1,34 @@
+function ShowPopup(victoryType, textKey)
+    -- See textpopup.lua in the game's source code to see how this is used
+    Events.SerialEventGameMessagePopup( {
+        Type = ButtonPopupTypes.BUTTONPOPUP_VICTORY_PROGRESS,
+        Data1 = victoryType,
+        Text = textKey
+    } );
+
+    -- ButtonPopupTypes.BUTTONPOPUP_QUEST_LOG
+    -- ButtonPopupTypes.BUTTONPOPUP_QUEST_OBJECTIVE_RECEIVED
+end
+
+function OnTechResearched(teamType, techType, change)
+    print("(Beyond Earth Eclipse) OnTechResearched")
+
+    local activePlayer
+    if Game.GetActivePlayer() ~= nil then
+        activePlayer = Players[Game.GetActivePlayer()]
+    else
+        activePlayer = Players[0]
+    end
+    if activePlayer:GetTeam() == teamType and activePlayer:IsHuman() then
+        if techType == GameInfo.Technologies["TECH_TRANSGENICS"].ID then
+            print("(Beyond Earth Eclipse) showing popup for transgenics")
+            ShowPopup("QUEST_VICTORY_TRANSCENDENCE", "TXT_KEY_QUEST_VICTORY_TRANSCENDENCE_TRANS_EPILOGUE")
+        elseif techType == GameInfo.Technologies["TECH_SWARM_INTELLIGENCE"].ID then
+            ShowPopup("QUEST_VICTORY_TRANSCENDENCE", "TXT_KEY_QUEST_VICTORY_TRANSCENDENCE_SWARM_EPILOGUE")
+        elseif techType == GameInfo.Technologies["TECH_NANOROBOTICS"].ID then
+            ShowPopup("QUEST_VICTORY_TRANSCENDENCE", "TXT_KEY_QUEST_VICTORY_TRANSCENDENCE_NANO_EPILOGUE")
+        end
+    end
+end
+
+GameEvents.TeamTechResearched.Add(OnTechResearched)
--- src/UI/VictoryPopup.lua
+++ src/UI/VictoryPopup.lua
@@ -1,93 +1,87 @@
+include( "IconSupport" );
+
 -------------------------------------------------
--- Plot Bonus Popup
+-- Victory Progress Popup
 -------------------------------------------------
 
+local m_PopupInfo = nil;
+
+-- TODO: is this needed? remove it and see if it breaks anything
+-- Since this is an enum, it just has to be a unique number; I just generated a random 6-digit number 🤷‍♂️
+ButtonPopupTypes.BUTTONPOPUP_VICTORY_PROGRESS = 823485;
+
 function OnPopup( popupInfo )
-    if(popupInfo.Type == ButtonPopupTypes.BUTTONPOPUP_PLOT_BONUS) then
-		local playerID = popupInfo.Data1;
-		local x = popupInfo.Data2;
-		local y = popupInfo.Data3;
-		local fromExpedition = popupInfo.Option1;
-		local player = Players[playerID];
-		if (player ~= nil and player:HasPlotBonusMessage(x, y) and not OptionsManager.IsNoRewardPopups() ) then
-			local messageTextTable = player:RetrieveAndRemovePlotBonusMessageText(x, y);
-			DisplayPopup(fromExpedition, messageTextTable);
-		end
-    end
+	if( popupInfo.Type ~= ButtonPopupTypes.BUTTONPOPUP_VICTORY_PROGRESS ) then
+		return;
+	end
+
+	local victoryType = popupInfo.Data1;
+
+	Controls.AlphaAnim:SetToBeginning();
+	Controls.AlphaAnim:Play();
+
+	-- Find the corresponding quest entry
+	local questInfo = GameInfo.Quests[victoryType];
+
+	-- TODO: title text isn't working; it's saying "Gifts from Home"
+	-- Set the title based on the victory type
+	if questInfo then
+		Controls.TitleText:SetText(Locale.ConvertTextKey(questInfo.Description));  -- Localized title
+	end
+
+	-- TODO: image isn't working
+	-- Set the image based on the victory type
+	if questInfo and questInfo.IconAtlas and questInfo.PortraitIndex then
+		IconHookup(questInfo.PortraitIndex, 128, questInfo.IconAtlas, Controls.Image);
+	end
+
+	-- Set the description text
+	Controls.BodyText:LocalizeAndSetText(popupInfo.Text);
+
+	-- local textSizeY = Controls.DetailsLabel:GetSizeY();
+	-- Controls.Content:SetSizeY(textSizeY + 275);
+
+	-- Controls.Content:ReprocessAnchoring();
+
+	UIManager:QueuePopup(ContextPtr, PopupPriority.InGameUtmost);
 end
 Events.SerialEventGameMessagePopup.Add( OnPopup );
 
 
-
-function DisplayPopup(fromExpedition, messageTextTable)
-	Controls.AlphaAnim:SetToBeginning();
-	Controls.AlphaAnim:Play();
-
-	-- Add generic window title so user knows where the data is coming from. This should be separate for each type (goody hut, or expedition site)
-	local title : string = "";
-	if (fromExpedition) then
-		title = Locale.ConvertTextKey("{TXT_KEY_EXPEDITION_COMPLETE:upper}");
-		Controls.Banner:SetTexture("Popup_Image_Expedition.dds");
-	else
-		title = Locale.ConvertTextKey("{TXT_KEY_RESOURCE_POD_SEARCHED:upper}");
-		Controls.Banner:SetTexture("Popup_Image_Resource_Pod.dds");
-	end
-	Events.AudioPlay2DSound("AS2D_INTERFACE_TECH_WINDOW");
-	local details = "";
-	if (messageTextTable.Details ~= nil) then
-		details = Locale.ConvertTextKey(messageTextTable.Details);
-	end
-
-	Controls.TitleLabel:SetText(title);
-	Controls.DetailsLabel:SetText(details);
-
-	local textSizeY : number = Controls.DetailsLabel:GetSizeY();
-	Controls.Content:SetSizeY(textSizeY + 275);
-
-	Controls.Content:ReprocessAnchoring();
-
-	UIManager:QueuePopup(ContextPtr, PopupPriority.InGameUtmost);
+----------------------------------------------------------------
+-- Input processing
+----------------------------------------------------------------
+function OnCloseButtonClicked ()
+    UIManager:DequeuePopup( ContextPtr );
 end
+Controls.CloseButton:RegisterCallback( Mouse.eLClick, OnCloseButtonClicked );
 
+
+-------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 function InputHandler( uiMsg, wParam, lParam )
     if uiMsg == KeyEvents.KeyDown then
         if wParam == Keys.VK_ESCAPE or wParam == Keys.VK_RETURN then
-            ClosePopup();
+            OnCloseButtonClicked();
             return true;
         end
     end
 end
-ContextPtr:SetInputHandler(InputHandler);
+ContextPtr:SetInputHandler( InputHandler );
 
-function ClosePopup()
-	UIManager:DequeuePopup(ContextPtr);
-end
-Controls.CloseButton:RegisterCallback(Mouse.eLClick, ClosePopup);
 
 -------------------------------------------------------------------------------
 -------------------------------------------------------------------------------
-
-local s_Shown = false;
-
 function ShowHideHandler( bIsHide, bInitState )
+
     if( not bInitState ) then
         if( not bIsHide ) then
-			if( not s_Shown ) then
-				s_Shown = true;
-        		UI.incTurnTimerSemaphore();
-        		Events.SerialEventGameMessagePopupShown(m_PopupInfo);
-				print("PlotBonusPopup, Blur On");
-				Events.BlurStateChange(0);
-			end
+        	UI.incTurnTimerSemaphore();
+        	Events.SerialEventGameMessagePopupShown(m_PopupInfo);
         else
-			if( s_Shown ) then
-				s_Shown = false;
-				UI.decTurnTimerSemaphore();
-				Events.SerialEventGameMessagePopupProcessed.CallImmediate(ButtonPopupTypes.BUTTONPOPUP_PLOT_BONUS, 0);
-				Events.BlurStateChange(1);
-				print("PlotBonusPopup, Blur Off");
-			end
-		end
+            UI.decTurnTimerSemaphore();
+            Events.SerialEventGameMessagePopupProcessed.CallImmediate( ButtonPopupTypes.BUTTONPOPUP_VICTORY_PROGRESS, 0 );
+        end
     end
 end
 ContextPtr:SetShowHideHandler( ShowHideHandler );
@@ -95,9 +89,4 @@ ContextPtr:SetShowHideHandler( ShowHideHandler );
 ----------------------------------------------------------------
 -- 'Active' (local human) player has changed
 ----------------------------------------------------------------
-function OnActivePlayerChanged( iActivePlayer, iPrevActivePlayer )
-	if (not ContextPtr:IsHidden()) then
-		ClosePopup();
-	end
-end
-Events.GameplaySetActivePlayer.Add(OnActivePlayerChanged);
+Events.GameplaySetActivePlayer.Add(OnCloseButtonClicked);
