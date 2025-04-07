d34782868a1470bca7fb1cc7ceac239a57e289fa
bmaupin <bmaupin@users.noreply.github.com>
2025-04-07 13:06:52 -0400

    feat: Track victory quest objectives
    ... for victory notifications. This should hopefully be a lot more robust than tracking the specific implementations (tech researched, project built, etc):
    
    - We don't have to know about the implementation, so maybe it could even work with mods that modify victory objectives
    - We don't have to associate each victory step with its notification and hard-code it
    - It will be easier than digging through the logic for each victory condition; in particular some of the victory logic would've been hard to track (e.g. launch a satellite, or the contact victory first stage which requires 2 out of 3 conditions to be met)

--- src/Lua/Victories.lua
+++ src/Lua/Victories.lua
@@ -4,30 +4,217 @@ function ShowPopup(victoryType, textKey)
         Data1 = GameInfo.Quests[victoryType].ID,
         Text = textKey
     } );
-
-    -- ButtonPopupTypes.BUTTONPOPUP_QUEST_LOG
-    -- ButtonPopupTypes.BUTTONPOPUP_QUEST_OBJECTIVE_RECEIVED
 end
 
-function OnTechResearched(teamType, techType, change)
-    print("(Beyond Earth Eclipse) OnTechResearched")
+-- function OnTechResearched(teamType, techType, change)
+--     print("(Beyond Earth Eclipse) OnTechResearched")
 
-    local activePlayer
-    if Game.GetActivePlayer() ~= nil then
-        activePlayer = Players[Game.GetActivePlayer()]
-    else
-        activePlayer = Players[0]
-    end
-    if activePlayer:GetTeam() == teamType and activePlayer:IsHuman() then
-        if techType == GameInfo.Technologies["TECH_TRANSGENICS"].ID then
-            print("(Beyond Earth Eclipse) showing popup for transgenics")
-            ShowPopup("QUEST_VICTORY_TRANSCENDENCE", "TXT_KEY_QUEST_VICTORY_TRANSCENDENCE_TRANS_EPILOGUE")
-        elseif techType == GameInfo.Technologies["TECH_SWARM_INTELLIGENCE"].ID then
-            ShowPopup("QUEST_VICTORY_TRANSCENDENCE", "TXT_KEY_QUEST_VICTORY_TRANSCENDENCE_SWARM_EPILOGUE")
-        elseif techType == GameInfo.Technologies["TECH_NANOROBOTICS"].ID then
-            ShowPopup("QUEST_VICTORY_TRANSCENDENCE", "TXT_KEY_QUEST_VICTORY_TRANSCENDENCE_NANO_EPILOGUE")
+--     local activePlayer
+--     if Game.GetActivePlayer() ~= nil then
+--         activePlayer = Players[Game.GetActivePlayer()]
+--     else
+--         activePlayer = Players[0]
+--     end
+--     if activePlayer:GetTeam() == teamType and activePlayer:IsHuman() then
+--         if techType == GameInfo.Technologies["TECH_TRANSGENICS"].ID then
+--             print("(Beyond Earth Eclipse) showing popup for transgenics")
+--             ShowPopup("QUEST_VICTORY_TRANSCENDENCE", "TXT_KEY_QUEST_VICTORY_TRANSCENDENCE_TRANS_EPILOGUE")
+--         elseif techType == GameInfo.Technologies["TECH_SWARM_INTELLIGENCE"].ID then
+--             ShowPopup("QUEST_VICTORY_TRANSCENDENCE", "TXT_KEY_QUEST_VICTORY_TRANSCENDENCE_SWARM_EPILOGUE")
+--         elseif techType == GameInfo.Technologies["TECH_NANOROBOTICS"].ID then
+--             ShowPopup("QUEST_VICTORY_TRANSCENDENCE", "TXT_KEY_QUEST_VICTORY_TRANSCENDENCE_NANO_EPILOGUE")
+--         end
+--     end
+-- end
+-- GameEvents.TeamTechResearched.Add(OnTechResearched)
+
+-- local contactVictoryStage = 0;
+-- local emancipationVictoryStage = 0;
+-- local promisedLandVictoryStage = 0;
+-- local transcendenceVictoryStage = 0;
+
+-- function UpdateVictoryStages()
+--     local player = Players[Game.GetActivePlayer()]
+
+--     if not player:IsHuman() or not player:IsAlive() then
+--         return;
+--     end
+
+--     local quests = player:GetQuests()
+
+--     for i, quest in ipairs(quests) do
+--         if quest:GetType() == "QUEST_VICTORY_CONTACT" then
+--             contactVictoryStage = quest:GetPersistentData().Stage
+--         elseif quest:GetType() == "QUEST_VICTORY_EMANCIPATION" then
+--             emancipationVictoryStage = quest:GetStage()
+--         elseif quest:GetType() == "QUEST_VICTORY_PROMISED_LAND" then
+--             promisedLandVictoryStage = quest:GetStage()
+--         elseif quest:GetType() == "QUEST_VICTORY_TRANSCENDENCE" then
+--             transcendenceVictoryStage = quest:GetStage()
+--         end
+--     end
+
+
+-- end
+
+-- Events.SequenceGameInitComplete.Add(UpdateVictoryStages);
+
+-- local victoryObjectives;
+-- function UpdateVictoryObjectives()
+--     local player = Players[Game.GetActivePlayer()]
+
+--     if not player:IsHuman() or not player:IsAlive() then
+--         return;
+--     end
+
+--     local quests = player:GetQuests()
+
+--     for i, quest in ipairs(quests) do
+--         if quest:GetType() == "QUEST_VICTORY_CONTACT" then
+--             victoryObjectives = quest:GetObjectives()
+--         elseif quest:GetType() == "QUEST_VICTORY_EMANCIPATION" then
+--             victoryObjectives = quest:GetObjectives()
+--         elseif quest:GetType() == "QUEST_VICTORY_PROMISED_LAND" then
+--             victoryObjectives = quest:GetObjectives()
+--         elseif quest:GetType() == "QUEST_VICTORY_TRANSCENDENCE" then
+--             victoryObjectives = quest:GetObjectives()
+--         end
+--     end
+
+-- end
+
+-- Events.SequenceGameInitComplete.Add(UpdateVictoryObjectives);
+
+
+
+-- function ShowVictoryStagePopup(victoryType, stage)
+--     local textKey = "TXT_KEY_QUEST_VICTORY_" .. victoryType .. "_STAGE_" .. stage
+--     ShowPopup(victoryType, textKey)
+-- if contactVictoryStage > 0 then
+--     ShowPopup("QUEST_VICTORY_CONTACT", "TXT_KEY_QUEST_VICTORY_CONTACT_STAGE_" .. contactVictoryStage)
+-- end
+
+-- if emancipationVictoryStage > 0 then
+--     ShowPopup("QUEST_VICTORY_EMANCIPATION", "TXT_KEY_QUEST_VICTORY_EMANCIPATION_STAGE_" .. emancipationVictoryStage)
+-- end
+
+-- if promisedLandVictoryStage > 0 then
+--     ShowPopup("QUEST_VICTORY_PROMISED_LAND", "TXT_KEY_QUEST_VICTORY_PROMISED_LAND_STAGE_" .. promisedLandVictoryStage)
+-- end
+
+-- if transcendenceVictoryStage > 0 then
+--     ShowPopup("QUEST_VICTORY_TRANSCENDENCE", "TXT_KEY_QUEST_VICTORY_TRANSCENDENCE_STAGE_" .. transcendenceVictoryStage)
+-- end
+-- end
+
+
+-- Track which victory objectives have been completed. We're indexing them by epilogue
+-- text key since this seems to be unique across victory objectives
+local completedVictoryObjectives = {}
+
+-- DELETEME
+local function printTable(t, indent)
+    indent = indent or ""
+    for k, v in pairs(t) do
+        if type(v) == "table" then
+            print(indent .. k .. " = {")
+            printTable(v, indent .. "  ")
+            print(indent .. "}")
+        else
+            print(indent .. k .. " = " .. tostring(v))
         end
     end
 end
 
-GameEvents.TeamTechResearched.Add(OnTechResearched)
+function PopulateCompletedVictoryObjectives()
+    for playerID = 0, GameDefines.MAX_CIV_PLAYERS - 1 do
+        local player = Players[playerID];
+
+        if not player:IsHuman() or not player:IsAlive() then
+            return;
+        end
+
+        -- DELETEME
+        print("(Beyond Earth Eclipse) === populating completed victory objective IDs " .. playerID .. " ===")
+
+        local quests = player:GetQuests()
+        for _, quest in ipairs(quests) do
+            local questId = quest:GetType()
+            local questInfo = GameInfo.Quests[questId];
+            local questType = questInfo.Type;
+
+            if (quest:IsInProgress() and
+                (questType == "QUEST_VICTORY_CONTACT" or
+                questType == "QUEST_VICTORY_EMANCIPATION" or
+                questType == "QUEST_VICTORY_PROMISED_LAND" or
+                questType == "QUEST_VICTORY_TRANSCENDENCE")
+            ) then
+                -- DELETEME
+                print("(Beyond Earth Eclipse) questType = " .. questType)
+
+                local objectives = quest:GetObjectives()
+                for _, objective in ipairs(objectives) do
+                    local objectiveEpilogue = objective:GetEpilogue()
+
+                    if ((not objective:IsInProgress()) and
+                        objective:DidSucceed() and
+                        not completedVictoryObjectives[objectiveEpilogue]
+                    ) then
+                        completedVictoryObjectives[objectiveEpilogue] = true;
+                    end
+                end
+            end
+        end
+    end
+
+    -- DELETEME
+    print("(Beyond Earth Eclipse) completedVictoryObjectiveIds=", "    ")
+    printTable(completedVictoryObjectives)
+end
+-- Run this once when the game is first started/loaded
+Events.SequenceGameInitComplete.Add(PopulateCompletedVictoryObjectives);
+
+function ShowVictoryPopup(playerID)
+    local player = Players[playerID];
+    if not player:IsHuman() or not player:IsAlive() then
+        return;
+    end
+
+    print("(Beyond Earth Eclipse) === Turn for Player " .. playerID .. " ===")
+
+    local quests = player:GetQuests()
+    for _, quest in ipairs(quests) do
+        local questId = quest:GetType()
+        local questInfo = GameInfo.Quests[questId];
+        local questType = questInfo.Type;
+
+        if (quest:IsInProgress() and
+            (questType == "QUEST_VICTORY_CONTACT" or
+            questType == "QUEST_VICTORY_EMANCIPATION" or
+            questType == "QUEST_VICTORY_PROMISED_LAND" or
+            questType == "QUEST_VICTORY_TRANSCENDENCE")
+        ) then
+            -- DELETEME
+            print("(Beyond Earth Eclipse) questType = " .. questType)
+
+            local objectives = quest:GetObjectives()
+            for _, objective in ipairs(objectives) do
+                local objectiveEpilogue = objective:GetEpilogue()
+
+                if ((not objective:IsInProgress()) and
+                    objective:DidSucceed() and
+                    not completedVictoryObjectives[objectiveEpilogue]
+                ) then
+                    -- TODO: do we need to exclude the quest epilogue objectives?, e.g. TXT_KEY_QUEST_VICTORY_TRANSCENDENCE_WAIT_EPILOGUE
+                    -- TODO: do we need to include the quest wait summary? e.g. TXT_KEY_QUEST_VICTORY_TRANSCENDENCE_WAIT_EPILOGUE
+                    print("(Beyond Earth Eclipse) showing popup for victory " .. questType .. " objective " .. objective:GetSummary())
+                    completedVictoryObjectives[objectiveEpilogue] = true;
+                    print("(Beyond Earth Eclipse) completedVictoryObjectiveIds=", "    ")
+                    printTable(completedVictoryObjectives)
+                    ShowPopup(questType, objectiveEpilogue);
+                end
+            end
+        end
+    end
+end
+GameEvents.PlayerDoTurn.Add(ShowVictoryPopup);
