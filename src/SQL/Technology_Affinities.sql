0f741fb31e1473aaca23d956e86bfece90497dbf
bmaupin <bmaupin@users.noreply.github.com>
2025-05-28 13:49:33 -0400

    feat: Re-scale flavours to a max of 9
    It seems that the max flavour value is 10 but the game can randomise it within 2 fewer or additional points. So if it's set to 12, no matter if the game adds or subtracts 2, it will always end up at 10 (the max value). By setting it to 9, in practice it means 7-10. So this gives a strong inclination but with a small amount of room for variation.
    
    Also add some commented code from an attempt to influence leaders to pick techs that would increase their affinity

--- src/SQL/Technology_Affinities.sql
+++ src/SQL/Technology_Affinities.sql
@@ -9,3 +9,17 @@ UPDATE Technology_Affinities
 SET AffinityValue = 27
 WHERE AffinityValue = 20
     AND EXISTS (SELECT Description FROM Civilizations WHERE Type = 'CIVILIZATION_CHUNGSU');
+
+-- More testing, this didn't seem to make a difference either
+-- INSERT INTO Technology_Flavors (TechType, FlavorType, Flavor)
+-- SELECT
+--     a.TechType,
+--     CASE a.AffinityType
+--         WHEN 'AFFINITY_TYPE_HARMONY' THEN 'FLAVOR_HARMONY'
+--         WHEN 'AFFINITY_TYPE_PURITY' THEN 'FLAVOR_PURITY'
+--         WHEN 'AFFINITY_TYPE_SUPREMACY' THEN 'FLAVOR_SUPREMACY'
+--     END AS FlavorType,
+--     20 AS Flavor
+-- FROM Technology_Affinities a
+-- WHERE a.AffinityValue = 27
+--   AND a.AffinityType IN ('AFFINITY_TYPE_HARMONY', 'AFFINITY_TYPE_PURITY', 'AFFINITY_TYPE_SUPREMACY');
