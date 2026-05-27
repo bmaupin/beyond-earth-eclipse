388d767ac46d34df6eb0bec0a76487e1c66faa66
bmaupin <bmaupin@users.noreply.github.com>
2026-05-27 13:26:00 -0400

    feat: Remove strategic resource overrides
    ... when Mini Beyond Earth is enabled; the logic has been moved to that mod since it's possible to add resources to the map after the game has already started so there's no need to override map generation scripts

--- src/Gameplay/Lua/AssignStartingPlots.lua
+++
@@ -1,9 +0,0 @@
--- There are significant differences in AssignStartingPlots.lua between the base game and
--- Rising Tide. Rather than try to merge them, both files have been added to this project
--- and are conditionally included.
-local isRisingTideActive = ContentManager.IsActive("54D2B257-C591-4045-8F17-A69F033166C7", ContentType.GAMEPLAY);
-if isRisingTideActive then
-	include("AssignStartingPlots-RisingTide");
-else
-	include("AssignStartingPlots-BaseGame");
-end
